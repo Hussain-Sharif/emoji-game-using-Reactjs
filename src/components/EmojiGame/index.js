6c66d7473026b707a3ea82a8506ef81661489d37
Hussain_Sharif <122522406+Hussain-Sharif@users.noreply.github.com>
2023-09-16 16:09:26 +0530

    Update index.js

--- src/components/EmojiGame/index.js
+++ src/components/EmojiGame/index.js
@@ -17,28 +17,72 @@ const shuffledEmojisList = () => {
 
 // Write your code here.
 
+class EmojiGame extends Component {
+  state = {
+    totalScore: 0,
+    topScore: 0,
+    activeGame: true,
+    clickedEmojis: [],
+  }
 
-class EmojiGame extends Component{
-    state={
-        totalScore:0,
-        topScore:0,
-        activeGame:true
-    }
+  clickEmoji = id => {
+    this.setState(prev => {
+      const {totalScore, clickedEmojis} = prev
+      const {emojisList} = this.props
+      const len = emojisList.length // length of original List
+      const check = clickedEmojis.includes(id) // Cheking :}
 
-    render(){
-        const {emojisList}=this.props;
-        const {totalScore,topScore,activeGame}=this.state;
-        return(
-            <div className="main-bg">
-                <NavBar totalScore={totalScore} topScore={topScore} activeGame={activeGame}/>
-                <div className="playground">
-                    {emojisList.map(each=>(
-                        <EmojiCard key={each.id} each={each}/>
-                    ))}
-                </div>
-            </div>
-        )
-    }
+      if (check === false) {
+        const added = [...clickedEmojis, id] // adding into NewList
+        const sum = added.reduce((a, b) => a + b) // Summing the List id's
+        if (sum < (len * (len + 1)) / 2) {
+          return {
+            totalScore: totalScore + 1,
+            clickedEmojis: added,
+          }
+        }
+        return {
+          totalScore: 0,
+          topScore: totalScore + 1,
+          clickedEmojis: added,
+          activeGame: false,
+        }
+      }
+      return {
+        totalScore: 0,
+        topScore: totalScore,
+        activeGame: false,
+        clickedEmojis: [],
+      }
+    })
+  }
+
+  render() {
+    let {emojisList} = this.props
+    const {totalScore, topScore, activeGame} = this.state
+    emojisList =
+      totalScore > 0 ? emojisList.sort(() => Math.random() - 0.5) : emojisList
+    return (
+      <div className="main-bg">
+        <NavBar
+          totalScore={totalScore}
+          topScore={topScore}
+          activeGame={activeGame}
+        />
+        <div className="card">
+          <ul className="playground">
+            {emojisList.map(each => (
+              <EmojiCard
+                key={each.id}
+                each={each}
+                clickEmoji={this.clickEmoji}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
-default export EmojiGame
+export default EmojiGame
