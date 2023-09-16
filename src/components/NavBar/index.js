1171d3eb9ce101bf31666e5a71329ef4eca38af6
Hussain_Sharif <122522406+Hussain-Sharif@users.noreply.github.com>
2023-09-16 16:10:45 +0530

    Conditional Class-name is added

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,19 +2,28 @@
 import {Component} from 'react'
 import './index.css'
 
-class NavBar extends Component{
-    render(){
-        const {totalScore,topScore,activeGame}=this.props
-        return(
-            <nav className=`{'navbar' ${activeGame?'flex-change':null} }`>
-                <div className="nav-logo-div">
-                    <img alt="emoji logo" src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png" className="logo"/>
-                    <p className="logo-head">Emoji Game</p>
-                </div>
-                {activeGame?<div className="score-div"><p className="score-details">Score: {totalScore}</p><p className="score-details">Top Score: {topScore}</p></div>:null}
-            </nav>
-        )
-    }
+class NavBar extends Component {
+  render() {
+    const {totalScore, topScore, activeGame} = this.props
+    return (
+      <nav className={`navbar ${activeGame ? null : 'flex-change'} `}>
+        <div className="nav-logo-div">
+          <img
+            alt="emoji logo"
+            src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+            className="logo"
+          />
+          <p className="logo-head">Emoji Game</p>
+        </div>
+        {activeGame ? (
+          <div className="score-div">
+            <p className="score-details">Score: {totalScore}</p>
+            <p className="score-details">Top Score: {topScore}</p>
+          </div>
+        ) : null}
+      </nav>
+    )
+  }
 }
 
-default export NavBar
+export default NavBar
