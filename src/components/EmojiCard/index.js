447a7e02aebf821b04600cf4b57fd0e76b554dd7
Hussain_Sharif <122522406+Hussain-Sharif@users.noreply.github.com>
2023-09-16 16:12:15 +0530

    Update-1 Emoji-card index.js

--- src/components/EmojiCard/index.js
+++ src/components/EmojiCard/index.js
@@ -1,1 +1,23 @@
 // Write your cod
+import {Component} from 'react'
+import './index.css'
+
+class EmojiCard extends Component {
+  shuffle = () => {
+    const {clickEmoji, each} = this.props
+    clickEmoji(each.id)
+  }
+
+  render() {
+    const {each} = this.props
+    return (
+      <li className="emo-li">
+        <button onClick={this.shuffle} type="button" className="emo-btn">
+          <img className="image" src={each.emojiUrl} alt={each.emojiName} />
+        </button>
+      </li>
+    )
+  }
+}
+
+export default EmojiCard
